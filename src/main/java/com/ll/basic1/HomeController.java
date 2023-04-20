26557e6cbf2ce00de09a0ec682d0464b0a643d44
calixlock <calixlock@naver.com>
2023-04-20 11:24:51 +0900

    controller_main_update_test

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -6,10 +6,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+    @GetMapping("/home/main")
 
     @ResponseBody
     public String showMain(){
-        return "안녕하세요";
+        return "main";
+    }
+    @GetMapping("/home/main2")
+
+    @ResponseBody
+    public String showMain2(){
+        return "main2 ";
+    }
+    @GetMapping("/home/main3")
+
+    @ResponseBody
+    public String showMain3(){
+        return "main3 ";
     }
 }
